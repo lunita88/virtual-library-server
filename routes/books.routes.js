ff6a3683d57cefd6a73139b388f35dce0ac5ac6a
lunita88 <68535269+lunita88@users.noreply.github.com>
2020-11-30 15:49:46 +0100

    Update books.routes.js
    added  muler and path and make change on routes for create book

--- routes/books.routes.js
+++ routes/books.routes.js
@@ -2,8 +2,21 @@
 module.exports = app => {
     const books = require("../controllers/books.controller.js");
   
+    const multer  = require('multer');
+    const path = require('path');
+    
+    var storage = multer.diskStorage({
+      destination: function (req, file, cb) {
+        cb(null, 'uploads/')
+      },
+      filename: function (req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname));
+      }
+    })
+    const upload = multer({ storage:storage});
+
     // Create a new book
-    app.post("/book", books.create);
+    app.post("/book", upload.single("book_img"),books.create);
   
      //Retrieve all books
     app.get("/books", books.findAll);
