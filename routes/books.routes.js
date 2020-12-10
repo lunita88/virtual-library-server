8c9a9d90eb88e853fc8ddc7a0839b3a2587fc0ad
lunita88 <68535269+lunita88@users.noreply.github.com>
2020-12-10 01:34:30 +0100

    Update books.routes.js
    put a middleware function in create book,delete and update

--- routes/books.routes.js
+++ routes/books.routes.js
@@ -1,6 +1,7 @@
 
 module.exports = app => {
     const books = require("../controllers/books.controller.js");
+    const users = require("../controllers/user.controller.js");
   
     const multer  = require('multer');
     const path = require('path');
@@ -9,26 +10,27 @@ module.exports = app => {
       destination: function (req, file, cb) {
         cb(null, 'uploads/')
       },
-      filename: function (req, file, cb) {
+        
+     filename: function (req, file, cb) {
         cb(null, Date.now() + path.extname(file.originalname));
       }
     })
     const upload = multer({ storage:storage});
 
     // Create a new book
-    app.post("/book", upload.single("book_img"),books.create);
+    app.post("/book", [users.authenticateToken, upload.single("book_img")],books.create);
   
      //Retrieve all books
     app.get("/books", books.findAll);
 
     //Delete a book with bookId
-    app.delete("/book/:bookId", books.delete);
+    app.delete("/book/:bookId", users.authenticateToken, books.delete);
   
      //Retrieve a single book with bookId
     app.get("/book/:bookId", books.findOne);
   
      //Update a book with bookId
-    app.put("/book/:bookId", books.update);
+    app.put("/book/:bookId",[users.authenticateToken, upload.single("book_img")], books.update);
   
      
   /*
